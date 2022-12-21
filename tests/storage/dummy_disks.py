9f2a55b4304a7cda3ee5e0ee3a1efa3a29581947
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-12-21 10:30:31 +0100

    type ignore bby

--- tests/storage/dummy_disks.py
+++ tests/storage/dummy_disks.py
@@ -24,8 +24,8 @@ def cleanup_loop_devices() -> None:
 
 
 def get_loop_devices_setup_config(arguments: list[str]) -> tuple[int | None, str | None]:
-    n_disks: int = int(arguments[0]) if arguments else DEFAULT_NUMBER_OF_DISKS
-    size: str = arguments[1] if len(arguments) > 1 else DEFAULT_DISK_SIZE
+    n_disks: int | None = int(arguments[0]) if arguments else DEFAULT_NUMBER_OF_DISKS
+    size: str | None = arguments[1] if len(arguments) > 1 else DEFAULT_DISK_SIZE
 
     if n_disks < 1:  # type: ignore
         DISKS_SETUP_LOG.error("You must provide a number of disks greater than 0")
