62fd6aaad2a5b49e527ebc690d850927c7935cdc
kamil.rybacki <kamil.rybacki@pg.edu.pl>
2022-12-23 10:01:17 +0100

    checkk github

--- .github/workflows/cache/index.ts
+++ .github/workflows/cache/index.ts
@@ -1,4 +1,5 @@
 
-module.exports = async ({context}) => {
+module.exports = async ({context, github}) => {
   console.log('The payload is: ' + JSON.stringify(context.payload));
+  console.log(github)
 };
